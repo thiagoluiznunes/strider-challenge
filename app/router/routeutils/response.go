ad1b18d52af2a0e8c3c2d726fe60d755bf3553d8
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2022-02-14 09:38:39 -0300

    feat: create server and its dependencies

---
+++ app/router/routeutils/response.go
@@ -0,0 +1,52 @@
+package routeutils
+
+import (
+	"net/http"
+	"strider-challenge/infra/exception"
+
+	"github.com/labstack/echo/v4"
+)
+
+// resultWrapper has fields for standard message responses
+type resultWrapper struct {
+	Error   bool        `json:"error,omitempty"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+// ResponseAPIOK returns a standard API success response
+func ResponseAPIOK(c echo.Context, data interface{}) error {
+	return c.JSON(http.StatusOK, data)
+}
+
+// ResponseAPIError returns a standard API error to the response
+func ResponseAPIError(c echo.Context, status int, message string) error {
+
+	returnValue := resultWrapper{
+		Error:   true,
+		Message: message,
+	}
+
+	return c.JSON(status, returnValue)
+}
+
+// HandleAPIError applies the default error handling to the response
+func HandleAPIError(c echo.Context, errorToHandle error) error {
+
+	statusCode := http.StatusServiceUnavailable
+	errorMessage := "Service Unavailable"
+
+	if errorToHandle != nil {
+		errorString := errorToHandle.Error()
+
+		switch e := errorToHandle.(type) {
+		case *exception.GeneralError:
+			statusCode = e.Code
+			errorMessage = errorString
+		default:
+			errorMessage = errorString
+		}
+	}
+
+	return ResponseAPIError(c, statusCode, errorMessage)
+}
---
+++ infra/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"github.com/mitchellh/mapstructure"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ServerPort  int  `mapstructure:"SERVER_PORT"`
+	ServerDebug bool `mapstructure:"SERVER_DEBUG"`
+}
+
+// initConfig reads in config file and ENV variables if set.
+func Read() (config Config, err error) {
+
+	var cfg Config
+	err = setViperDefaults(cfg)
+	if err != nil {
+		return config, err
+	}
+
+	viper.SetTypeByDefaultValue(true)
+	viper.AddConfigPath(".")
+	viper.SetConfigFile(".env")
+
+	viper.ReadInConfig()
+	viper.AutomaticEnv()
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return config, err
+	}
+
+	return cfg, err
+}
+
+func setViperDefaults(cfg Config) (err error) {
+
+	cfgMap := make(map[string]interface{})
+	err = mapstructure.Decode(cfg, &cfgMap)
+	if err != nil {
+		return err
+	}
+
+	err = viper.MergeConfigMap(cfgMap)
+
+	return err
+}
